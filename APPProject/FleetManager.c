238a494713b434bbc29f772e59d53f560f252098
ISHA YOUNAS <G00427804@atu.ie>
2025-04-12 11:03:53 +0100

    Adding general starting function prototypes

--- APPProject/FleetManager.c
+++ APPProject/FleetManager.c
@@ -4,7 +4,65 @@
 #include<stdlib.h> // malloc
 #include<string.h> // strings
 
+// structure for linked list
+typedef struct node
+{
+	int data;
+	struct node* NEXT;
+
+}nodeT;
+
+// function prototypes
+void addMachine(nodeT** top);
+void displayToScreen();
+void displayMachineDetails(nodeT* top);
+void updateMachine(nodeT* top);
+void deleteMachine(nodeT* top);
+void genearteStatistics(nodeT* top);
+void saveToFile();
+void listMachines(nodeT* top);
+
 int main()
 {
 
 } // main
+
+void addMachine(nodeT** top)
+{
+
+} // addMachine
+
+void displayToScreen()
+{
+
+} // displayToScreen
+
+void displayMachineDetails(nodeT* top)
+{
+
+} // displayMachineDetails
+
+void updateMachine(nodeT* top)
+{
+
+} // updateMachine
+
+void deleteMachine(nodeT* top)
+{
+
+} // deleteMachine
+
+void genearteStatistics(nodeT* top)
+{
+
+} // genearteStatistics
+
+void saveToFile()
+{
+
+} // saveToFile
+
+void listMachines(nodeT* top)
+{
+
+} // listMachines
